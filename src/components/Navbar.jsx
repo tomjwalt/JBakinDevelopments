2342cf3a3531d6c9c409b8a4f04001b90f819ae1
tomjwalt <tomjwalton123@gmail.com>
2025-01-26 20:49:27 +0000

    final commit
    - added flex to the navbar
    -cleaned up white space
    -bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,12 +18,13 @@ const Navbar = () => {
   return (
     <header className="navbar">
       <div className="contact">
-        <Mailto email="jbakindevelopments@hotmail.com">jbakindevelopments@hotmail.com</Mailto>
-        <Callto phone="07827028940">07827028940</Callto>
+        <Mailto className="contact-text" email="jbakindevelopments@hotmail.com">jbakindevelopments@hotmail.com</Mailto>
+        <Callto className="contact-text" phone="07827028940">07827028940</Callto>
       </div>
       <div className="logo-container">
         <img className="logo-img" src={Logo} alt="Logo" />
       </div>
+      <div className="facebook-logo-container"></div>
       <a href="https://www.facebook.com/profile.php?id=61556660973348">
         <img className="facebook-logo" src={FacebookLogo} alt="facebook-logo" />
       </a>
